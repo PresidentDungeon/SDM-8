651706d2b0d41159e713abf09748d4256848a725
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2020-10-27 11:53:07 +0100

    Initial commit

---
+++ finalProjectRegistrationv2-develop/Interfaces/IStudentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Model;
+using System.Collections.Generic;
+
+namespace Interfaces
+{
+    public interface IStudentRepository
+    {
+        IEnumerable<Student> GetAll();
+        Student GetById(int id);
+        void Add(Student s);
+        void Update(Student s);
+        void Remove(Student s);
+    }
+}
