632b7f35b642c2b3b4c5c68669735ad069868539
Jackei94 <55141365+Jackei94@users.noreply.github.com>
2020-10-29 10:08:16 +0100

    Created create teams

---
+++ finalProjectRegistrationv2-develop/Interfaces/ITeamRepository.cs
@@ -0,0 +1,16 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    public interface ITeamRepository
+    {
+        IEnumerable<Team> GetAll();
+        Team GetById(int id);
+        void Add(Team t);
+        void Update(Team t);
+        void Remove(Team t);
+    }
+}
---
+++ finalProjectRegistrationv2-develop/Services/TeamService.cs
@@ -0,0 +1,38 @@
+﻿using Interfaces;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class TeamService
+    {
+        private ITeamRepository TeamRepository;
+
+
+        public TeamService(ITeamRepository teamRepository)
+        {
+            this.TeamRepository = TeamRepository;
+        }
+
+        public Team createTeam(int id, string teamName)
+        {
+            if (string.IsNullOrEmpty(teamName)){
+                throw new ArgumentException("Team name can't be null or empty");
+            }
+
+            if(this.TeamRepository.GetById(id) != null)
+            {
+                throw new ArgumentException("Team with same ID already exists");
+            }
+
+            if(id < 0)
+            {
+                throw new ArgumentException("ID value can't be negative");
+            }
+
+            return new Team { ID = id, Name = teamName, Students = new List<Student>()};
+        }
+    }
+}
